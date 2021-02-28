3b6e6d2cc76010c607cd604f7b903fdd72ebd7e1
SHAKTI TRIPATHI <shaktithemastermind@gmail.com>
2021-02-28 19:15:20 +0530

    bottom navigation

---
+++ some_assignment-master/bottom_nav/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bottom_nav/nav.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bottom Navigation Bar Tutorial',
+      home: Nav(),
+    );
+  }
+}
---
+++ some_assignment-master/bottom_nav/lib/message.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Message extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("This is message screen"),
+    );
+  }
+}
