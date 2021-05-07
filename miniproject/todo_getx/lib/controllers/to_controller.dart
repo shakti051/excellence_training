e6da0fd1eafb375ccc5d8782d80078b812655ba1
shakti051 <shaktithemastermind@gmail.com>
2021-05-07 16:16:34 +0530

    todo added

--- miniproject/todo_getx/lib/controllers/to_controller.dart
+++ miniproject/todo_getx/lib/controllers/to_controller.dart
@@ -3,19 +3,18 @@ import 'package:get_storage/get_storage.dart';
 import 'package:todo_getx/models/todo.dart';
 
 class TodoController extends GetxController {
-
-  var todos = List<Todo>().obs;
-  
+  //var todos = List<Todo>().obs;
+  var todos = [].obs;
   @override
   void onInit() {
-    super.onInit();
     List storedTodos = GetStorage().read<List>('todos');
-    if(storedTodos!=null){
-        todos = storedTodos.map((e) => Todo.fromJson(e)).toList().obs;
+    if (storedTodos != null) {
+      todos = storedTodos.map((e) => Todo.fromJson(e)).toList().obs;
     }
 
     ever(todos, (_) {
       GetStorage().write('todos', todos.toList());
     });
+    super.onInit();
   }
 }
